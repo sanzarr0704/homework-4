5b46fa3a4e66a0f5836f75c274fdf9f9846c0745
Sanjar Saparov <117997805+sanzarr0704@users.noreply.github.com>
2023-01-28 00:35:34 +0600

    Это дз 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// контролируемый компонент
+
+NameForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('Отправленное имя: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Имя:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Отправить" />
+      </form>
+    );
+  }
 }
+
+// не контролируемый компонент
+
+// class NameForm extends React.Component {
+  // constructor(props) {
+//     super(props);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//     this.input = React.createRef();
+//   }
+
+//   handleSubmit(event) {
+//     alert('Отправленное имя: ' + this.input.current.value);
+//     event.preventDefault();
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Имя:
+//           <input type="text" ref={this.input} />
+//         </label>
+//         <input type="submit" value="Отправить" />
+//       </form>
+//     );
+//   }
+// }
 
 export default App;
